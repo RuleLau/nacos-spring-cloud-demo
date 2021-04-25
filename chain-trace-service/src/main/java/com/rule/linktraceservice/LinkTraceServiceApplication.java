94a1698b8af0b6a91486278678295ab0bd5054ca
RuleLau <863915498@qq.com>
2021-04-25 23:19:50 +0800

    feat: add service module

---
+++ chain-trace-service/src/main/java/com/rule/linktraceservice/LinkTraceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rule.linktraceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkTraceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkTraceServiceApplication.class, args);
+	}
+
+}
---
+++ chain-trace-service/src/test/java/com/rule/linktraceservice/LinkTraceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rule.linktraceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkTraceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

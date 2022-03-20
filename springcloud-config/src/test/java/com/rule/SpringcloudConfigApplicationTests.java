7aa088e1c131f35b62a71ef5a5bd8b5ab636dd83
RuleLau <863915498@qq.com>
2022-03-21 00:22:59 +0800

    feat: add spring-cloud-config 使用

---
+++ springcloud-config/src/test/java/com/rule/SpringcloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
